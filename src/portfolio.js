1c9d2a7c8307d404cf5d2dc1e4128172926d82fb
Keenan Lee-Peters <96672628+klp1130@users.noreply.github.com>
2022-07-28 07:06:53 -0600

    added to about props to portfolio

--- src/portfolio.js
+++ src/portfolio.js
@@ -3,5 +3,16 @@ const header = {
     title: 'KLP.',
 }
 
+const about = {
+    name: 'Keenan Lee-Peters',
+    role: 'Full Stack Engineer', 
+    description: 'I am a full stack developer & designer. I love building things that work great and look amazing. I enjoy using my attention to detail, orderliness and problem-solving abilities to make things that help people.',
+    resume: 'somewhere',
+    Social: {
+        linkedin: 'https://www.linkedin.com/in/keenanlee-peters/',
+        github: 'https://github.com/klp1130/'
 
-export { header }
+    }
+}
+
+export { header, about }
