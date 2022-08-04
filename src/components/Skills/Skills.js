0f8db2f9be821a0015e86ec3bafe80f43dd2e6e8
Keenan Lee-Peters <keenan.leepeters@gmail.com>
2022-08-04 11:00:34 -0700

    95 PERCENT

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -6,7 +6,7 @@ const Skills = () => {
     if (!skills.length) return null
 
     return (
-        <section id='skills'>
+        <section className='section skills' id='skills'>
             <h2 className='section__title'>Skills</h2>
             <ul className='skills__list'>
                 {skills.map((skill) =>(
