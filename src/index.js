8b4e1238010829df79c48008142fb72aecc667f7
Keenan Lee-Peters <keenan.leepeters@gmail.com>
2022-08-04 09:47:30 -0700

    index.css added to root

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOMClient from 'react-dom/client'
 import App from './App'
 import React from 'react'
+import './index.css'
 
 const root = ReactDOMClient.createRoot(document.getElementById('root'))
 root.render(
