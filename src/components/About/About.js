1c16962e108fd0ffe1497603104df0b00edf06af
Keenan Lee-Peters <keenan.leepeters@gmail.com>
2022-08-04 08:58:36 -0700

    css added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,7 @@
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import { about } from '../../portfolio'
+import './About.css'
 
 const About = () => {
     const { name, role, description, resume, social } = about
@@ -9,17 +10,17 @@ const About = () => {
         <div>
             { name && (
                 <h1>
-                    Hi, I am <span>{name}.</span>
+                    Hi, I am <span className='about__name'>{name}.</span>
                 </h1>
             )}
 
-            {role && <h2>A {role}.</h2>}
-            <p>{description && description}</p>
+            {role && <h2 className='about__role'>A {role}.</h2>}
+            <p className='about__desc'>{description && description}</p>
             
             <div>
                 {resume && (
                     <a href={resume}>
-                        <span type='button'>
+                        <span type='button' className='btn btn--outline'>
                             Resume
                         </span>
                     </a>
@@ -29,6 +30,7 @@ const About = () => {
                 <> 
                 <a
                   href={social.github}
+                  className='link link--icon'
                 >
                   <GitHubIcon />
                 </a>
@@ -37,6 +39,7 @@ const About = () => {
             {social && (   
                 <a
                   href={social.linkedin}
+                  className='link link--icon'
                 >
                   <LinkedInIcon />
                 </a>
