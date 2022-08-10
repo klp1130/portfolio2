eb395394ca807a046c83efa7ccdd9ee6a4dc70f8
Keenan Lee-Peters <96672628+klp1130@users.noreply.github.com>
2022-08-10 08:25:46 -0600

    comma after gumroad

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,7 +20,7 @@ const projects = [
     {
         name: 'BrualNotes', 
         description:
-            'I took some established speech recognition code written in vanilla Javascript and expanded its functionality by refactoring it into a react app, setting up a JSON database for storage of the notes and added styling. The style is influenced by the current design trend of neubrutalism (hence brutal notes). This style is used by companies like Gumroad.com Figma and MongoDB.',
+            'I took some established speech recognition code written in vanilla Javascript and expanded its functionality by refactoring it into a react app, setting up a JSON database for storage of the notes and added styling. The style is influenced by the current design trend of neubrutalism (hence brutal notes). This style is used by companies like Gumroad.com, Figma and MongoDB.',
         stack: ['React'],
         sourceCode: 'https://github.com/klp1130/brutalnotes',
         livePreview: 'https://klp1130.github.io/brutalnotes/'
