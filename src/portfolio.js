73eafcc2606e2e9dfd33e81c5759f067640ea54b
Keenan Lee-Peters <96672628+klp1130@users.noreply.github.com>
2022-08-23 08:38:15 -0600

    updated url for Dash UI

--- src/portfolio.js
+++ src/portfolio.js
@@ -23,7 +23,7 @@ const projects = [
             'ArcanaLife inc needed a single place where all of the tasks and tools for running the business could be easily located and used. With this dashboard ArcanaLife and its employees can improve the performance of the company, utilize their data more efficiently and generate better ROI. This project was built with React.',
         stack: ['React','Tailwind',],
         sourceCode: 'https://github.com/klp1130/dashboard',
-        livePreview: 'https://6303997e474de00e6ab0437c--admirable-puppy-c1b1a0.netlify.app/'
+        livePreview: 'https://admirable-puppy-c1b1a0.netlify.app/'
             
     },{
         name: 'BrualNotes', 
